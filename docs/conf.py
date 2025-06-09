99ee08583ba9e2206481a61395625d5ba7724a66
uhc-card <jamesthompseon@gmail.com>
2025-06-09 15:26:42 +0530

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+project = 'UHC Card'
+author = 'UHC Card'
+release = '1.0.0'
+
+extensions = []
+templates_path = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+master_doc = 'index'  # ✅ Optional but recommended
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_extra_path = ['.']
+
+html_show_sourcelink = False
+html_theme_options = {
+    'show_powered_by': False,
+}
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+.. raw:: html
+
+   <script>
+     if (window.location.pathname === "/en/latest/") {
+       window.location.href = "index.html";
+     }
+   </script>
